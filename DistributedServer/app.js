365123075201c12b0897d3e45cd77fcd4efc3e37
hj5230 <hf305@foxmail.com>
2023-04-23 16:01:43 +0300

    Prepare dependencies

--- DistributedServer/app.js
+++ DistributedServer/app.js
@@ -11,8 +11,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    
+app.get('/test', (req, res) => {
+    res.json('ok')
 })
 
 module.exports = app;
