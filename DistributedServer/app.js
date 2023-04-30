c1d25a7e966a3326fc882d5d109abf7522f72666
hj5230 <hf305@foxmail.com>
2023-04-30 14:20:45 +0300

    weather done

--- DistributedServer/app.js
+++ DistributedServer/app.js
@@ -1,18 +1,50 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require('cors');
 
-var app = express();
+const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
-app.get('/test', (req, res) => {
-    res.json('ok')
-})
+app.get("/gettemp", async (req, res) => {
+  import("node-fetch").then(({ default: fetch }) => {
+    (async () => {
+      const now = new Date();
+      const year = now.getFullYear();
+      const month = String(now.getMonth() + 1).padStart(2, "0");
+      const day = String(now.getDate()).padStart(2, "0");
+      const hour = String(now.getHours()).padStart(2, "0");
+      const datetime = `${year}-${month}-${day}T${hour}:00`;
+      const url =
+        "https://api.open-meteo.com/v1/forecast?latitude=60.17&longitude=24.94&hourly=temperature_2m";
+      const response = await fetch(url);
+      const data = await response.json();
+      const index = data.hourly.time.indexOf(datetime);
+      const temp = data.hourly.temperature_2m[index];
+      res.send({
+        temp: temp,
+      });
+    })();
+  });
+});
+
+app.post("/getwiki", async (req, res) => {
+  const { item } = req.body
+  import("node-fetch").then(({ default: fetch }) => {
+    (async() => {
+      const wikiPro = await fetch(
+        `https://en.wikipedia.org/api/rest_v1/page/summary/${item}`
+      );
+      const wikiRes = await wikiPro.json();
+      const extractHTML = wikiRes.extract_html;
+      res.json(extractHTML)
+    })();
+  })
+});
 
 module.exports = app;
